ff6c552c41508c2afd495bc44213c928b6455b28
Kurabey <mrtcanvural@gmail.com>
2021-10-09 14:00:11 +0300

    added first week's notes

---
+++ 1.py
@@ -0,0 +1,37 @@
+# import math
+#
+# var = math.ceil(5 / 2)
+# print(var)
+## inputun yerine geçeer
+#
+# var = input()
+# print(var)
+# print(type(var))
+## \ yazmak için 2 \
+## \t -> tab \n -> new_line
+#
+#
+# print("AA\tBB\nCC")
+## r harfi olduğu gibi okutur
+##print(r"AA\tBB\nCC")
+
+print("AA", "BB", sep="--", end="")
+print("AA", "BB", sep="--", end="")
+print("afaogansog oagjaosgıs".format())
+x = input()
+y = input()
+var = "aaaa{0}bbbbb{1}ccccc"
+print(var.format(f"{x}", f"{y}"))
+
+#from L0810 import Variables
+#import Variables
+
+# importing folders
+
+
+
+#3.
+
+#Branching
+
+
